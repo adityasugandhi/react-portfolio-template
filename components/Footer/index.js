4a9531c82cd1deea44229aa57b16d4bed651c348
adityasugandhi <39901010+adityasugandhi@users.noreply.github.com>
2024-02-05 23:37:03 -0500

    Updated json

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -25,7 +25,7 @@ const Footer = ({}) => {
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
         Software Engineer{" "}
-        <Link href="adityasugandhi.com">
+        <Link href="adityasugandhi.netlify.app">
           <a className="underline underline-offset-1">Aditya Sugandhi</a>
         </Link>
       </h1>
