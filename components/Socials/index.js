39ceb17f58e2d105d0951e2093a44b4df2553f79
adityasugandhi <39901010+adityasugandhi@users.noreply.github.com>
2024-03-26 15:01:34 +0000

    added socials

--- components/Socials/index.js
+++ components/Socials/index.js
@@ -1,14 +1,22 @@
 import React from "react";
 import Button from "../Button";
-
+import Image from 'next/image';
 import yourData from "../../data/portfolio.json";
+import {useTheme} from "next-themes";
 
 const Socials = ({ className }) => {
+  let { resolvedTheme } = useTheme();
+  console.log(resolvedTheme,"socials")
   return (
     <div className={`${className} flex flex-wrap mob:flex-nowrap link`}>
       {yourData.socials.map((social, index) => (
         <Button key={index} onClick={() => window.open(social.link)}>
-          {social.title}
+          <Image 
+            src={resolvedTheme === 'dark' ? social.logo_dark : social.logo} 
+            height={30} 
+            width={30} 
+            alt={social.title} 
+          />
         </Button>
       ))}
     </div>
